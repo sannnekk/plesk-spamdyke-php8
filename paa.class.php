b0efe0e99adc4ca95871dbd1b655e11ef40da69c
sannnekk <nukleoid@outlook.com>
2023-02-28 11:39:19 +0100

    Polyfil linked

--- paa.class.php
+++ paa.class.php
@@ -13,6 +13,8 @@ of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Publ
 You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>.
 */
 
+require __DIR__ . "/polyfills.php"
+
 class paa{
 
       
